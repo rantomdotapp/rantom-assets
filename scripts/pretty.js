4313ecacf46fbc233aa7a8ace120de4614125ee2
Noat Eden <noat.eth@gmail.com>
2023-05-06 20:11:47 +0700

    add protocols metadata

--- scripts/pretty.js
+++ scripts/pretty.js
@@ -1,7 +1,17 @@
 const fs = require('fs');
 
+const protocols = require('../metadata/protocols.json');
+
 // whe rename all token address to lowercase in tokens directory
 const files = fs.readdirSync('./tokens');
 for (const file of files) {
   fs.renameSync(`./tokens/${file}`, `./tokens/${file.toLowerCase()}`);
 }
+
+const ordered = Object.keys(protocols)
+  .sort()
+  .reduce((obj, key) => {
+    obj[key] = protocols[key];
+    return obj;
+  }, {});
+fs.writeFileSync('./metadata/protocols.json', JSON.stringify(ordered).toString());
